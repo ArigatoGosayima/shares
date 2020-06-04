bf768752ffd51e5a87ba822001ec352655eab2c1
ArigatoGosayima <63019472+ArigatoGosayima@users.noreply.github.com>
2020-06-04 17:01:11 +0100

    Update MagnumCrawler.py

--- MagnumCrawler.py
+++ MagnumCrawler.py
@@ -57,7 +57,7 @@ def letgocrawler(maxim):
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_argument('--proxy-server=%s' % PROXY)
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('user-agent=%s' %user_agent)
     options.add_argument('--no-sandbox')
