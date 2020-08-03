1315b7e8d50388e8e1cfc1a0b7cd879fff5a4679
ArigatoGosayima <63019472+ArigatoGosayima@users.noreply.github.com>
2020-08-03 04:43:28 +0100

    Update forwarder.py

--- forwarder.py
+++ forwarder.py
@@ -1,58 +1,30 @@
-import cryptocompare
-from telethon import events
-from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty
+import time
 
-api_id = 1277086  # your API ID
-api_hash = '10257d27ba3d4b8f5eb80b29f3d4e98a'  # Your API Hash
-phone = '+353863552124'  # your phone number
-client = TelegramClient(phone, api_id, api_hash)
-client.connect()
-if not client.is_user_authorized():
-    client.send_code_request(phone)
-    client.sign_in(phone, input('Enter the code: '))
+from selenium import webdriver
 
-chats = []
-last_date = None
-chunk_size = 200
-groups = []
+tel = []
+PROXY = '200.35.154.13:1212'
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('user-agent=%s' % user_agent)
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+chrome_prefs = {}
+options.experimental_options["prefs"] = chrome_prefs
+chrome_prefs["profile.default_content_settings"] = {"images": 2}
+chrome_prefs["profile.managed_default_content_settings"] = {"images": 2}
+driver = webdriver.Chrome(options=options)
 
-result = client(GetDialogsRequest(
-    offset_date=last_date,
-    offset_id=0,
-    offset_peer=InputPeerEmpty(),
-    limit=chunk_size,
-    hash=0
-))
-chats.extend(result.chats)
+driver.get('https://www.avito.ma/fr/marrakech/electorm%C3%A9nager_et_vaisselle/climat_9000_41397868.htm')
+try:
+    b = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/div/div[2]/button')
+    b.click()
 
-for chat in chats:
-    try:
-        groups.append(chat)
-    except:
-        continue
-print(groups)
-print('Choose a group to set Trace on:')
-i = 0
-for g in groups:
-    print(str(i) + ' - ' + g.title)
-    i += 1
-g_index = input("Enter a Number: ")
-target_group = groups[int(g_index)]
-print('Choose a group to give to:')
-i = 0
-for g in groups:
-    print(str(i) + ' - ' + g.title)
-    i += 1
-g_index = input("Enter a Number: ")
-home_group = groups[int(g_index)]
-print('Setting trace on: ' + str(target_group.title))
-print('Setting destination as: ' + str(home_group.title))
-message = ''
-while True:
-    @client.on(events.NewMessage(chats=target_group.id))
-    async def my_event_handler(event):
-        await client.forward_messages(home_group, event.message)
-    client.start()
-    client.run_until_disconnected()
+    c = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/div[2]/div/a/span/span')
+    tel.append(c.text)
+    print(c.text)
+except:
+    pass
